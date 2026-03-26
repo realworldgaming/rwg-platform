1cbcad165eb7dc67b31f5f1abf9c9e18111c6623
realworldgaming <rwgrealworldgaming@gmail.com>
2026-03-26 07:51:47 +0530

    fix: next.config.js for Next.js 14 compatibility

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["api.rawg.io", "media.rawg.io"],
+  },
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["api.rawg.io", "media.rawg.io"],
-  },
-};
-
-export default nextConfig;
